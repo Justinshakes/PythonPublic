5a7d6623ddf7f274b0c12ce691df74669539c277
Justinshakes <Superjustin44@gmail.com>
2023-06-29 16:10:54 -0700

    Update Tester.py

--- Courses/cs50P/lecture3/problemSet/Tester.py
+++ Courses/cs50P/lecture3/problemSet/Tester.py
@@ -1,19 +1,34 @@
-grocery_list = {}
+# Month names
+months = [
+    "January", "February", "March", "April", "May", "June", "July",
+    "August", "September", "October", "November", "December"
+]
 
 while True:
     try:
-        item = input().upper()
+        date_input = input("Enter a date (MM/DD/YYYY or Month Day, Year): ")
 
-        if item in grocery_list:
-            grocery_list[item] += 1
+        if "/" in date_input:
+            month, day, year = map(int, date_input.split("/"))
         else:
-            grocery_list[item] = 1
+            month_name, day, year = date_input.split()
 
-    except EOFError:
-        break
+            day = day[:-1]
 
-sorted_list = sorted(grocery_list.items(), key=lambda x: x[0])
+            month = months.index(month_name) + 1
 
-for item, count in sorted_list:
-    print(f"{count} {item}")
+            day = int(day)
+            month = int(month)
+            year = int(year)
 
+        if 1 <= month <= 12 and 1 <= day <= 31:
+            date_output = f"{year:0d}-{month:02d}-{day:02d}"
+
+            print(date_output)
+            break
+        else:
+            print("Invalid date. Please enter a valid date.")
+
+
+    except ValueError:
+        print("Invalid input. Please enter the date in the specified format.")
