3deef4896ef3d4a77597f2a5f295fce94362ac5b
Justinshakes <Superjustin44@gmail.com>
2023-06-29 15:53:00 -0700

    Update Tester.py

--- Courses/cs50P/lecture3/problemSet/Tester.py
+++ Courses/cs50P/lecture3/problemSet/Tester.py
@@ -1,29 +1,20 @@
-# Menu dictionary with menu items as keys and their prices as values
-Menu = {
-    "Baja Taco": 4.00,
-    "Burrito": 7.50,
-    "Bowl": 8.50,
-    "Nachos": 11.00,
-    "Quesadilla": 8.50,
-    "Super Burrito": 8.50,
-    "Super Quesadilla": 9.50,
-    "Taco": 3.00,
-    "Tortilla Salad": 8.00
-}
-
-total = 0.00
+grocery_list = {}
 
 while True:
     try:
-        order = input("Item: ").title()  # Prompt the user to enter the menu item they want to order
-        # Check if the entered item exists in the menu
-        if order in Menu:
-            total += Menu[order] # Add the price of the ordered item to the total
-            print("Total: $" + format(total, ".2f")) # Print total with 2 decimal places
-        else:
-            print("Invalid item. Please choose a valid item from the menu.")  # Inform the user about an invalid item
-    except EOFError:
-        break  # Exit the loop immediately when Ctrl+D (EOF) is encountered
+        item = input().upper()
 
-print("Program closed.")
+        if item in grocery_list:
+            grocery_list[item] += 1
+        else:
+            grocery_list[item] = 1
+
+    except EOFError:
+        break
+
+sorted_list = sorted(grocery_list.items(), key=lambda x: x[0])
+
+for item in grocery_list:
+    count = grocery_list[item]
+    print(count, item)
 
