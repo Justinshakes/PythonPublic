11ce669ff0c69a4617dd4385fb788a325ee33d97
Justinshakes <Superjustin44@gmail.com>
2023-06-29 13:34:57 -0700

    Update Tester.py

--- Courses/cs50P/lecture3/problemSet/Tester.py
+++ Courses/cs50P/lecture3/problemSet/Tester.py
@@ -1,16 +1,32 @@
-months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-]
+def main():
+    fraction = get_fracdtion()
+    percentage = get_percentage(fraction)
+    remaining_fuel(percentage)
 
-print(months[0])
+
+def get_fraction():
+    while True:
+        fraction = input("Fraction: ")
+        try:
+            x, y = map(int, fraction.split("/"))
+            result = round(eval(fraction), 2)
+            if 0 <= result <= 1:
+                return result
+        except (NameError, ZeroDivisionError, SyntaxError, ValueError):
+            pass
+
+
+def get_percentage(fraction):
+    return round(float(fraction) * 100)
+
+
+def remaining_fuel(percentage):
+    if percentage <= 1:
+        print("E")
+    elif percentage >= 99:
+        print("F")
+    else:
+        print(f"{percentage}%", end="")
+
+
+main()
