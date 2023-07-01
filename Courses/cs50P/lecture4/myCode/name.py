a34ce4fee5ec6289b9473a5af700f56c9ebee727
Justin Sherrell <Superjustin44@gmail.com>
2023-06-30 21:00:32 -0700

    Update name.py

--- Courses/cs50P/lecture4/myCode/name.py
+++ Courses/cs50P/lecture4/myCode/name.py
@@ -1,8 +1,8 @@
 import sys
 
-
+# Check for errors in command line arguments
 if len(sys.argv) < 2:
-    print("Too few arguments")
-elif len(sys.argv) > 2:
-    print("Too many arguments")
-print("hello, my name is ", sys.argv[1])
+    sys.exit("Too few arguments")
+
+for arg in sys.argv[1:]:
+    print(arg)
