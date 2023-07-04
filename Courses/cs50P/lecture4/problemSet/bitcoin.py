d8c92fdaa60b70a66d5438b2dc93286cb2c457d4
Justin Sherrell <Superjustin44@gmail.com>
2023-07-04 01:44:35 -0700

    Update bitcoin.py

--- Courses/cs50P/lecture4/problemSet/bitcoin.py
+++ Courses/cs50P/lecture4/problemSet/bitcoin.py
@@ -2,35 +2,22 @@ import json
 import requests
 import sys
 
-
 if len(sys.argv) == 1:
     sys.exit("Missing command-line argument")
-elif not (sys.argv[1]).isnumeric():
-    sys.exit("Command-line argument is not a number")
-
 
 try:
-    # Send a GET request to the Coindesk API
+    argument = float(sys.argv[1])
+except ValueError:
+    sys.exit("Command-line argument is not a valid number")
+
+try:
     response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
-
-    # Check if the request was successful (status code 200)
     if response.status_code == 200:
-        # Extract the JSON data from the response
         data = response.json()
-
-        # Print the JSON data with indentation for readability
-        # print(json.dumps(data, indent=2))
-
-        # Extract the USD float price from the JSON data
         usd_price = float(data['bpi']['USD']['rate_float'])
-
-        # Print the USD float price
-        total = usd_price * float(sys.argv[1])
+        total = usd_price * argument
         print(f"${total:,.4f}")
     else:
-        # Print an error message if the request was unsuccessful
         print("Error: Failed to retrieve data from the API.")
-
 except requests.RequestException as e:
-    # Print an error message if there was an exception during the request
     print("Error:", str(e))
