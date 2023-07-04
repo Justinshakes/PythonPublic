134d027d783b5114dc8db9eea7cdd421c5c1b12c
Justin Sherrell <Superjustin44@gmail.com>
2023-07-03 17:22:37 -0700

    Update Exercise11.py

---
+++ Exercises/Basic (Part I)/11 - 20/Exercise11.py
@@ -0,0 +1,15 @@
+"""
+11. Write a Python program to print the documents (syntax, description etc.) of Python built-in function(s).
+Sample function : abs()
+Expected Result :
+abs(number) -> number
+Return the absolute value of the argument.
+"""
+
+print("abs.__doc__: ", abs.__doc__)
+
+print("int.__doc__: ", int.__doc__)
+
+# print(input.__doc__)
+
+# print(print.__doc__)
