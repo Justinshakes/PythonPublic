b310c4cf4d65758a7a0e16d28502a4ff63adcd96
Justinshakes <Superjustin44@gmail.com>
2023-07-05 02:35:12 -0700

    Update Tester.py

--- Courses/cs50P/lecture4/problemSet/Tester.py
+++ Courses/cs50P/lecture4/problemSet/Tester.py
@@ -1,59 +1,10 @@
-import random
-
-def main():
-    level = get_level()
-    score = 0
-    counter = 0
-
-    while counter < 10:
-        errors_counter = 0
-        x = generate_integer(level)
-        y = generate_integer(level)
-        answer = x + y
-
-        while True:
-            try:
-                if errors_counter == 3:
-                    print(f"{x} + {y} = {answer}")
-                    counter += 1
-                    break
-
-                user_answer = int(input(f"{x} + {y} = "))
-                if user_answer == answer:
-                    score += 1
-                    counter += 1
-                    break
-                else:
-                    print("EEE")
-                    errors_counter += 1
-                    continue
-            except ValueError as e:
-                print("EEE")
-                errors_counter += 1
-                continue
-    print("Score: ", score)
+import json
+import requests
+import sys
 
 
-def get_level():
-    while True:
-        try:
-            level = int(input("Level: "))
-            if 1 <= level <= 3:
-                return level
-        except ValueError:
-            pass
+response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+if response.status_code == 200:
+    print(json.dumps(response.json(), indent=2))
 
 
-def generate_integer(level):
-    if level not in [1, 2, 3]:
-        raise ValueError("Invalid level")
-
-    if level == 1:
-        return random.randint(0, 9)
-    elif level == 2:
-        return random.randint(10, 99)
-    else:
-        return random.randint(100, 999)
-
-if __name__ == "__main__":
-    main()
