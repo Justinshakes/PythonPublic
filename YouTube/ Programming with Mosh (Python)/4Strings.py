712e30bd493b161ff3652ec326c07ca5a99227f1
Justinshakes <Superjustin44@gmail.com>
2023-07-06 16:12:44 -0700

    Create 4Strings.py

---
+++ YouTube/ Programming with Mosh (Python)/4Strings.py
@@ -0,0 +1,17 @@
+# course = 'Python for "Beginners"'
+# course = '''
+# Hi John,
+#
+# Here is our first email to you.
+#
+# Thank you,
+# The support team
+# '''
+
+# course = 'Python for Beginners'
+# another = course[:]
+# print(another)
+
+name = 'Jennifer'
+print(name[1:-1])
+# Output: ennife
