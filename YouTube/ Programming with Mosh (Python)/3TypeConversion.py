e2f9b8bc05458a4b755ccc2b211569b1cf8db070
Justinshakes <Superjustin44@gmail.com>
2023-07-06 16:04:13 -0700

    Update 3TypeConversion.py

--- YouTube/ Programming with Mosh (Python)/3TypeConversion.py
+++ YouTube/ Programming with Mosh (Python)/3TypeConversion.py
@@ -1,4 +1,19 @@
-birth_year = int(input('birth year: '))
-age = 2023 - birth_year
+# birth_year = input('birth year: ')
+# print(type(birth_year))
+# age = 2023 - int(birth_year)
+# print(type(age))
+#
+# print(age)
+#
+# num = 3.2
+# print(type(num))
 
-print(age)
+# Ask a user their weight (in pounds), convert
+# it to kilograms and print it
+
+weight_lbs = input("Weight (in pounds): ")
+
+weight_kg = round(int(weight_lbs) / 2.205, 2)
+
+print("Weight in Kilograms:", weight_kg)
+
