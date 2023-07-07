4fc7b66a4f927b03b5d4d71b696a1b5dfd2ec300
Justinshakes <Superjustin44@gmail.com>
2023-07-07 00:41:37 -0700

    update

--- YouTube/ Programming with Mosh (Python)/6StringMethods.py
+++ YouTube/ Programming with Mosh (Python)/6StringMethods.py
@@ -3,6 +3,10 @@ course = 'Python for Beginners'
 # print(len(course))
 # print(course.upper())
 # print(course.lower())
+# print(course.title())
 # print(course)
 
-print(course.find('n'))
+# print(course.find("B"))
+# print(course.replace("Beginners", "Absolute Beginners"))
+
+print('Python' in course)
