ba9c408ebe3ecb1b88bc040868beacc8d8cc4341
Justinshakes <Superjustin44@gmail.com>
2023-07-07 01:06:39 -0700

    Update 10IfStatements.py

---
+++ YouTube/ Programming with Mosh (Python)/10IfStatements.py
@@ -0,0 +1,23 @@
+# is_hot = False
+# is_cold = False
+#
+# if is_hot:
+#     print("It's a hot day")
+#     print("Drink plenty of water")
+# elif is_cold:
+#     print("It's a cold day")
+#     print("Wear warm clothes")
+# else:
+#     print("it's a lovely day")
+# print("Enjoy your day")
+
+house_price = 1000000
+good_credit = False
+
+if good_credit:
+    down_payment = house_price * 0.10
+else:
+    down_payment = house_price * 0.20
+
+print(f"Down payment: ${down_payment:,.2f}")
+
