8fe5e51f04494127c0de83ea249a27ae15753ef5
Justinshakes <Superjustin44@gmail.com>
2023-07-09 00:06:11 -0700

    cs50p

--- Courses/cs50P/lecture6/lectureCode/students/students.py
+++ Courses/cs50P/lecture6/lectureCode/students/students.py
@@ -6,5 +6,3 @@ home = input("Where's your home? ").strip()
 with open("students.csv", "a", newline="") as file:
     writer = csv.DictWriter(file, fieldnames=["name", "home"])
     writer.writerow({"name": name, "home": home})
-
-
