58cb3d4802c00f58ea3f1fd06805c8451c68d840
Justinshakes <Superjustin44@gmail.com>
2023-07-09 18:43:53 -0700

    update

--- Courses/cs50P/lecture3/problemSet/fuel.py
+++ Courses/cs50P/lecture3/problemSet/fuel.py
@@ -8,7 +8,6 @@ def get_fraction():
     while True:
         fraction = input("Fraction: ")   # Prompt the user to enter a fraction
         try:
-            x, y = map(int, fraction.split("/"))   # Split the fraction by "/" and convert the parts to integers
             result = round(eval(fraction), 2)      # Evaluate the fraction and round the result to 2 decimal places
             if 0 <= result <= 1:                   # Check if the result is between 0 and 1 (inclusive)
                 return result                      # Return the result if it meets the condition
