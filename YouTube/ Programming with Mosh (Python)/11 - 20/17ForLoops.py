1bc71a9f9a83a67f9cecb8b554bdcf0f6e376330
Justinshakes <Superjustin44@gmail.com>
2023-07-09 21:10:42 -0700

    Update 17ForLoops.py

---
+++ YouTube/ Programming with Mosh (Python)/11 - 20/17ForLoops.py
@@ -0,0 +1,20 @@
+# for item in "Python":
+#     print(item)
+
+# for item in ['Most', 'John', 'Sarah']:
+#     print(item)
+
+# for item in [1, 2, 3, 4]:
+#     print(item)
+
+#                start, end, step
+# for item in range(5, 10, 2):
+#     print(item)
+
+prices = [10, 20, 30]
+total = 0
+
+for price in prices:
+    total += price
+
+print(f"Total: ${total}")
