5d6a0f755f590f7a6bc2cdf0f9f493185a91d3c1
Justinshakes <Superjustin44@gmail.com>
2023-07-09 19:24:31 -0700

    Create review.py

---
+++ Courses/cs50P/lecture5/problem_set/twitter/review.py
@@ -0,0 +1,14 @@
+def main():
+    user_input = input("Input: ")
+    print(shorten(user_input))
+
+
+def shorten(word):
+    vowels = ['A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u']
+    result_string = ''
+    for char in word:
+        if char not in vowels:
+            result_string += char
+    return result_string
+
+main()
