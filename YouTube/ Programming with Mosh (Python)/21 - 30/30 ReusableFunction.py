deb8e78189a03d3053aa60937e41715f9d800027
Justinshakes <Superjustin44@gmail.com>
2023-07-11 15:28:08 -0700

    Create 30 ReusableFunction.py

---
+++ YouTube/ Programming with Mosh (Python)/21 - 30/30 ReusableFunction.py
@@ -0,0 +1,15 @@
+def emoji_converter(msg):
+    words = msg.split(' ')
+    emojis = {
+        ":)": "😀",
+        ":(": "☹️"
+    }
+
+    output = ''
+    for word in words:
+        output += emojis.get(word, word) + " "
+    return output
+
+
+message = input(">")
+print(emoji_converter(message))
