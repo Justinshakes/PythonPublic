acd4fa0135f12bd85737727141ec8ab7bfc72057
Justinshakes <Superjustin44@gmail.com>
2023-07-10 21:51:35 -0700

    Create 23Unpacking.py

---
+++ YouTube/ Programming with Mosh (Python)/21 - 30/23Unpacking.py
@@ -0,0 +1,12 @@
+coordinates = (1, 2, 3)
+# coordinates = [1, 2, 3]
+# x y z
+
+# x = coordinates[0]
+# y = coordinates[1]
+# z = coordinates[2]
+
+# unpacks
+x, y, z = coordinates
+
+print(x, y, z)
