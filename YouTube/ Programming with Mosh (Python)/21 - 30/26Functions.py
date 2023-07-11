1f57fbadc0e23bbf6535fa86ed67196cf8abac14
Justinshakes <Superjustin44@gmail.com>
2023-07-11 15:08:50 -0700

    Create 26Functions.py

---
+++ YouTube/ Programming with Mosh (Python)/21 - 30/26Functions.py
@@ -0,0 +1,14 @@
+# def main():
+#     print("Start")
+#     greet_user()
+#     print("Finish")
+
+def greet_user():
+    print("Hi there!")
+    print("Welcome aboard")
+
+
+print("Start")
+greet_user()
+print("Finish")
+# main()
