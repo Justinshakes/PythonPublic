d40a07f1444182d585f387b9b9ce331105d7ed0c
Justinshakes <Superjustin44@gmail.com>
2023-07-12 23:47:40 -0700

    Update pizza.py

--- Courses/cs50P/lecture6/problemset/pizza/pizza.py
+++ Courses/cs50P/lecture6/problemset/pizza/pizza.py
@@ -15,3 +15,29 @@ with open(sys.argv[1]) as file:
 
 print(tabulate(reader, headers="firstrow",
                tablefmt="grid"))
+
+"""
+Pizza 2.0 ?
+import csv
+import sys
+from tabulate import tabulate
+
+# Check if the correct command-line argument is provided
+if len(sys.argv) != 2 or not sys.argv[1].endswith(".csv"):
+    sys.exit("Invalid argument. Please provide a single .csv file.")
+
+# Open the CSV file and read its contents
+with open(sys.argv[1], newline="") as file:
+    reader = csv.reader(file)
+    data = list(reader)
+
+# Check if the CSV file is empty
+if not data:
+    sys.exit("The CSV file is empty.")
+
+# Print the CSV data in a tabular format
+headers = data[0]
+table_data = data[1:]
+print(tabulate(table_data, headers=headers, tablefmt="grid"))
+
+"""
