961f1d44e04ccfdb8bbc98497c2b8315bbb7b6c2
Justinshakes <Superjustin44@gmail.com>
2023-07-12 23:44:15 -0700

    Create review.py

---
+++ Courses/cs50P/lecture6/problemset/lines/review.py
@@ -0,0 +1,16 @@
+import sys
+
+if len(sys.argv) != 2 or not sys.argv[1].endswith(".py"):
+    sys.exit("Invalid argument, must be a single .py file")
+
+with open(sys.argv[1]) as file:
+    lines = file.readlines()
+
+code_line_count = 0
+
+for line in lines:
+    line = line.strip()
+    if not line.startswith('#' and line.strip()):
+        code_line_count += 1
+
+print(code_line_count)
