0fc70d3c77e18bd31fb3f457a5cfe050b0d34c88
Justinshakes <Superjustin44@gmail.com>
2023-07-14 15:54:27 -0700

    update

---
+++ Courses/cs50P/lecture6/problemset/shirt/shirt.py
@@ -0,0 +1,9 @@
+# pip install Pillow
+# You can open an image (e.g., shirt.png) with code like:
+# shirt = Image.open("shirt.png")
+
+# You can get the width and height, respectively, of that image as a tuple with code like:
+# size = shirt.size
+
+# And you can overlay that image on top of another (e.g., photo) with code like
+# photo.paste(shirt, shirt)
