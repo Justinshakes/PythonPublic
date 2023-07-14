6b7d94481a8bc79bc48a19ce9f7c31bf98a01312
Justinshakes <Superjustin44@gmail.com>
2023-07-14 15:51:31 -0700

    update

--- Courses/cs50P/lecture6/problemset/scourgify/scourgify.py
+++ Courses/cs50P/lecture6/problemset/scourgify/scourgify.py
@@ -1,42 +1,34 @@
+import sys
 import csv
 
-import csv
+if len(sys.argv) < 3:
+    sys.exit("Too few command-line arguments")
+elif len(sys.argv) > 3:
+    sys.exit("Too many command-line arguments")
 
-# Open the input CSV file
-with open("before.csv") as file:
-    # Open the output CSV file
-    with open("after.csv", 'w', newline='') as new_file:
-        # Specify the fieldnames for the output file
-        fieldnames = ["first", "last", "house"]
+try:
+    # Open the input CSV file
+    with open(sys.argv[1]) as file:
+        # Open the output CSV file
+        with open(sys.argv[2], 'w', newline='') as new_file:
+            # Specify the fieldnames for the output file
+            fieldnames = ["first", "last", "house"]
 
-        # Create a DictWriter object to write to the output file
-        writer = csv.DictWriter(new_file, fieldnames=fieldnames)
+            # Create a DictWriter object to write to the output file
+            writer = csv.DictWriter(new_file, fieldnames=fieldnames)
 
-        # Create a DictReader object to read from the input file
-        reader = csv.DictReader(file)
+            # Create a DictReader object to read from the input file
+            reader = csv.DictReader(file)
 
-        # Write the header row to the output file
-        writer.writeheader()
+            # Write the header row to the output file
+            writer.writeheader()
 
-        # Iterate over each row in the input file
-        for row in reader:
-            # Write the "name" field to the output file
-            last, first = row["name"].split(",")
-            # print("first:" + first + "  Last: ")
-            # writer.writerow({"first name": first_name, "last name": last_name, "house": row["house"]})
-            writer.writerow({"first": first, "last": last, "house": " " + row["house"]})
+            # Iterate over each row in the input file
+            for row in reader:
+                # Write the "name" field to the output file
+                last, first = row["name"].split(",")
+                writer.writerow({"first": first.strip(), "last": last.strip(), "house": row["house"]})
 
-# Print houses
-# import csv
-#
-# with open("old.csv") as file:
-#     reader = csv.DictReader(file)
-#     for row in reader:
-#         house = row["house"]
-#         print(house)
+except FileNotFoundError:
+    sys.exit("Could not read " + sys.argv[1])
 
-# Print whole row
-# with open("old.csv") as file:
-#     reader = csv.DictReader(file)
-#     for row in reader:
-#         print(row)
