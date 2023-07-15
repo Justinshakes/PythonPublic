a6b0fa601dc503c5f666e9e93a3a6ca4b6bc8060
Justinshakes <Superjustin44@gmail.com>
2023-07-14 19:24:20 -0700

    Update shirt.py

--- Courses/cs50P/lecture6/problemset/shirt/shirt.py
+++ Courses/cs50P/lecture6/problemset/shirt/shirt.py
@@ -6,6 +6,10 @@ def main():
 
 
 def check_command_line_arg():
+    if len(sys.argv) < 3:
+        sys.exit("Too few command-line arguments")
+    if len(sys.argv) > 3:
+        sys.exit("Too many command-line arguments")
     print(sys.argv)
 
 
