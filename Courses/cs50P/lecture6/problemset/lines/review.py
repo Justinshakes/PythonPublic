a8234f5b1e9ff5f1d9b509d92e7643c07cc5b133
Justin Sherrell <Superjustin44@gmail.com>
2023-07-16 17:31:12 -0700

    Update review.py

--- Courses/cs50P/lecture6/problemset/lines/review.py
+++ Courses/cs50P/lecture6/problemset/lines/review.py
@@ -1,7 +1,7 @@
 import sys
 
 if len(sys.argv) != 2 or not sys.argv[1].endswith(".py"):
-    sys.exit("Invalid arguments, must be a single .py file")
+    sys.exit("Invalid argument, must be a single .py file")
 
 with open(sys.argv[1]) as file:
     lines = file.readlines()
@@ -12,5 +12,4 @@ for line in lines:
     line = line.strip()
     if not line.startswith('#') and line.strip():
         code_line_count += 1
-
-print(code_line_count)
+        
