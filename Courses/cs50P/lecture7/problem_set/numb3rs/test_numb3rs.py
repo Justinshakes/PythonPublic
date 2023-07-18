c49e8d8a5098e3da9f65e8e1c9f5696eae6ecdd8
Justin Sherrell <Superjustin44@gmail.com>
2023-07-18 14:20:32 -0700

    update

---
+++ Courses/cs50P/lecture7/problem_set/numb3rs/test_numb3rs.py
@@ -0,0 +1,17 @@
+import pytest
+from numb3rs import validate
+
+
+def test_valid_ips():
+    assert validate("127.0.0.10") == True
+    assert validate("255.255.255.0") == True
+
+
+def test_invalid_letters_numbers():
+    assert validate("1Dog2") == False
+    assert validate("4Human6") == False
+
+
+def test_invalid_ips_out_of_range():
+    assert validate("275.3.6.28") == False
+    assert validate("-1.3.6.28") == False
