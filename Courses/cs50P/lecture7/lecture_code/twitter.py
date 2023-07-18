bd1d4f335171a0e2cb1e465535646b9d3260a9dc
Justin Sherrell <Superjustin44@gmail.com>
2023-07-18 13:41:47 -0700

    Update twitter.py

--- Courses/cs50P/lecture7/lecture_code/twitter.py
+++ Courses/cs50P/lecture7/lecture_code/twitter.py
@@ -2,6 +2,9 @@ import re
 
 url = input("URL: ").strip()
 
-username = re.sub(r"^https://twitter\.com/", "", url)
+if matches := re.search(r"^https?://(?:www\.)?twitter\.com/([a-z0-9_])+$", url, re.IGNORECASE):
+    print(f"Username:", matches.group(1))
 
-print(f"Username: {username}")
+# username = re.sub(r"(^https?//)?(www\.)?twitter\.com/", "", url)
+
+# print(f"Username: {username}")
