2c9395f768c8048387c58f3f2c2d178e25ad24cc
Justin Sherrell <Superjustin44@gmail.com>
2023-07-19 19:03:29 -0700

    Update watch.py

---
+++ Courses/cs50P/lecture7/problem_set/watch/watch.py
@@ -0,0 +1,18 @@
+import re
+import sys
+
+
+def main():
+    print(parse(input("HTML: ")))
+
+
+def parse(s):
+    if re.search(r"<iframe(.)*><\/iframe>", s):
+        
+
+
+...
+
+
+if __name__ == "__main__":
+    main()
