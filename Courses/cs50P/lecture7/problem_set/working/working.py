0f589e8a60cf2f25d8e02e9a0e5536109b8049ed
Justin Sherrell <Superjustin44@gmail.com>
2023-07-20 01:56:15 -0700

    Update working.py

--- Courses/cs50P/lecture7/problem_set/working/working.py
+++ Courses/cs50P/lecture7/problem_set/working/working.py
@@ -2,18 +2,21 @@ import re
 
 
 def main():
+    # Get user input for time and call the convert function to process it
     print(convert(input("Hours: ")))
 
 
 def convert(s):
+    # Check if the input matches the correct format using regular expression
     if correct_format := re.search(
             r"^(([0-2]*[0-9]):*([0-5][0-9])*) ([A|P]M) to (([0-2]*[0-9]):*([0-5][0-9])*) ([A|P]M)$", s):
 
+        return None
     else:
+        # If the input doesn't match the correct format, raise a ValueError
         raise ValueError
 
 
-...
-
+# Call the main function if the script is run directly
 if __name__ == "__main__":
     main()
