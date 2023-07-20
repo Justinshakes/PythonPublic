4b88edc3b961b28ef15e70ca649654a2cba91a55
Justin Sherrell <Superjustin44@gmail.com>
2023-07-20 01:52:20 -0700

    Update working.py

--- Courses/cs50P/lecture7/problem_set/working/working.py
+++ Courses/cs50P/lecture7/problem_set/working/working.py
@@ -6,11 +6,14 @@ def main():
 
 
 def convert(s):
-    ...
+    if correct_format := re.search(
+            r"^(([0-2]*[0-9]):*([0-5][0-9])*) ([A|P]M) to (([0-2]*[0-9]):*([0-5][0-9])*) ([A|P]M)$", s):
+
+    else:
+        raise ValueError
 
 
 ...
-
 
 if __name__ == "__main__":
     main()
