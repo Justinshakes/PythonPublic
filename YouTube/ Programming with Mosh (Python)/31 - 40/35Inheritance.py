abb322d827cb82e5b87b9af5508e9a6ab8001e88
Justin Sherrell <Superjustin44@gmail.com>
2023-07-21 23:18:35 -0700

    Create 35Inheritance.py

---
+++ YouTube/ Programming with Mosh (Python)/31 - 40/35Inheritance.py
@@ -0,0 +1,24 @@
+class Mammal:
+    def walk(self):
+        print("walk")
+
+
+class Dog(Mammal):
+    def bark(self):
+        print("Bark")
+
+
+class Cat(Mammal):
+    def meow(self):
+        print("Meow")
+
+
+d1 = Dog()
+
+c1 = Cat()
+
+d1.bark()
+d1.walk()
+
+c1.meow()
+c1.walk()
