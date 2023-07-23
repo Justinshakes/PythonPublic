54dbc3dace99528e261a05d29e8ee7bddfb68ae8
Justin Sherrell <Superjustin44@gmail.com>
2023-07-23 02:15:05 -0700

    Update student.py

--- Courses/cs50P/lecture8(OOP)/lecture_code/student.py
+++ Courses/cs50P/lecture8(OOP)/lecture_code/student.py
@@ -1,11 +1,12 @@
 def main():
     student = get_student()
+    if student["name"] == "Padma":
+        student["house"] = "Ravenclaw"
     print(f"{student['name']} from {student['house']}")
 
 
 def get_student():
-    student = {"name": input("Name: "), "house": input("House: ")}
-    return student
+    return {"name": input("Name: "), "house": input("House: ")}
 
 
 if __name__ == "__main__":
