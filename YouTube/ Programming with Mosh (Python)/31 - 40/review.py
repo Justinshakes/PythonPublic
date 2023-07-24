8283d0411064ea7880417b26044960136160e937
Justin Sherrell <Superjustin44@gmail.com>
2023-07-24 00:53:30 -0700

    Create review.py

---
+++ YouTube/ Programming with Mosh (Python)/31 - 40/review.py
@@ -0,0 +1,21 @@
+class Point:
+    def move(self):
+        print("Move")
+
+    def draw(self):
+        print("Draw")
+
+
+point1 = Point()
+
+point1.draw()
+point1.move()
+
+point1.x = 10
+point1.y = 20
+
+print(point1.x)
+print(point1.y)
+
+
+
