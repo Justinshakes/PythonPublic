f32b6af4cb0996481a9a42af7e92a43360ffd144
Justin Sherrell <Superjustin44@gmail.com>
2023-07-29 19:06:06 -0700

    Update lecture_notes.py

---
+++ Courses/cs50P/lecture8(OOP)/lecture_code/lecture_notes.py
@@ -0,0 +1,14 @@
+# I will get to this I WILL!
+def main():
+    student = get_student()
+    if student["name"] == "Padma":
+        student["house"] = "Ravenclaw"
+    print(f"{student['name']} from {student['house']}")
+
+
+def get_student():
+    return {"name": input("Name: "), "house": input("House: ")}
+
+
+if __name__ == "__main__":
+    main()
