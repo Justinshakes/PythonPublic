5e661a989680157dbc47120e3000a9407dd1651c
Justin Sherrell <Superjustin44@gmail.com>
2023-07-30 23:57:31 -0700

    Create review.py

---
+++ Courses/cs50P/lecture8(OOP)/lecture_code/review.py
@@ -0,0 +1,16 @@
+class Student:
+    ...
+
+def main():
+    student = get_student()
+    print(student)
+
+
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return Student(name, house)
+
+
+if __name__ == "__main__":
+    main()
