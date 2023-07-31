9d7ae4d120b011eecb09600e88635f88a5459e59
Justin Sherrell <Superjustin44@gmail.com>
2023-07-31 00:03:39 -0700

    Update review.py

--- Courses/cs50P/lecture8(OOP)/lecture_code/review.py
+++ Courses/cs50P/lecture8(OOP)/lecture_code/review.py
@@ -1,5 +1,32 @@
 class Student:
-    ...
+    def __init__(self, name, house):
+        self.name = name
+        self.house = house
+
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if not name:
+            raise ValueError("Missing name")
+        self._name = name
+
+    @property
+    def house(self):
+        return self._house
+
+    @house.setter
+    def house(self, house):
+        valid_houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin", "farm"]
+        if house not in valid_houses:
+            raise ValueError(f"{house} is an invalid house")
+        self._house = house
+
 
 def main():
     student = get_student()
