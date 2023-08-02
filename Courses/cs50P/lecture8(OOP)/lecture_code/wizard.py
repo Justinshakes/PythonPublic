dfff19c1415d68eae27d84a265771cef86d2ba50
Justin Sherrell <Superjustin44@gmail.com>
2023-08-02 05:03:14 -0700

    Update wizard.py

--- Courses/cs50P/lecture8(OOP)/lecture_code/wizard.py
+++ Courses/cs50P/lecture8(OOP)/lecture_code/wizard.py
@@ -6,14 +6,21 @@ class Wizard:
 
 
 class Student(Wizard):
-    def __init(self, name, house):
+    def __init__(self, name, house):
+        super().__init__(name)
         self.house = house
 
-        ...
+        # ...
 
 
 class Professor(Wizard):
-    def __init(self, name, subject):
+    def __init__(self, name, subject):
+        super().__init__(name)
         self.subject = subject
 
-        ...
+        # ...
+
+
+wizard = Wizard("Albus")
+student = Student("Harry", "Gryffindor")
+professor = Professor("Severus", "Defense Against the Dark Arts")
