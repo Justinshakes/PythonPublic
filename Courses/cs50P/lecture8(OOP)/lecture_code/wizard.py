5c3fbc2ee907fce1c5ae12eabbec798494ae4dcd
Justinshakes <Superjustin44@gmail.com>
2023-08-03 15:35:09 -0700

    Update wizard.py

--- Courses/cs50P/lecture8(OOP)/lecture_code/wizard.py
+++ Courses/cs50P/lecture8(OOP)/lecture_code/wizard.py
@@ -1,8 +1,16 @@
+import random
+
+
 class Wizard:
+    houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+
     def __init__(self, name):
         if not name:
             raise ValueError("Missing name")
         self.name = name
+
+    def __str__(self):
+        return f"{self.name} is a wizard"
 
 
 class Student(Wizard):
@@ -10,7 +18,8 @@ class Student(Wizard):
         super().__init__(name)
         self.house = house
 
-        # ...
+    def __str__(self):
+        return f"{self.name} is a student from house {self.house}"
 
 
 class Professor(Wizard):
@@ -18,9 +27,14 @@ class Professor(Wizard):
         super().__init__(name)
         self.subject = subject
 
-        # ...
+    def __str__(self):
+        return f"{self.name} is a Professor that teaches {self.subject}"
 
 
 wizard = Wizard("Albus")
 student = Student("Harry", "Gryffindor")
 professor = Professor("Severus", "Defense Against the Dark Arts")
+
+print(wizard)
+print(student)
+print(professor)
