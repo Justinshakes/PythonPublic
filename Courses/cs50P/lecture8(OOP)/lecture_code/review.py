2c81c491c7725b8190409098df1c409433c5ef01
Justinshakes <Superjustin44@gmail.com>
2023-08-03 15:43:48 -0700

    Update review.py

--- Courses/cs50P/lecture8(OOP)/lecture_code/review.py
+++ Courses/cs50P/lecture8(OOP)/lecture_code/review.py
@@ -1,24 +1,14 @@
-class Vault:
-    def __init__(self, galleons=0, sickles=0, knuts=0):
-        self.galleons = galleons
-        self.sickles = sickles
-        self.knuts = knuts
+print(type(50))
+print(type(int()))
 
-    def __str__(self):
-        return f"{self.galleons} Galleons, {self.sickles} Sickles, {self.knuts} Knuts"
+print(type("Str"))
+print(type(str()))
 
-    def __add__(self, other):
-        galleons = self.galleons + other.galleons
-        sickles = self.sickles + other.sickles
-        knuts = self.knuts + other.knuts
-        return Vault(galleons, sickles, knuts)
+print(type([1, 2, 3]))
+print(type(list()))
 
+print(type({"apple": 3}))
+print(type(dict()))
 
-potter = Vault(100, 50, 25)
-print(potter)
-
-weasley = Vault(25, 50, 100)
-print(weasley)
-
-combined = potter + weasley
-print(combined)
+print(type((1, 2, 3)))
+print(type(tuple()))
