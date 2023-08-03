897869705039f0ab521f4a082699c8ff18fa0f30
Justinshakes <Superjustin44@gmail.com>
2023-08-03 15:41:33 -0700

    Update review.py

--- Courses/cs50P/lecture8(OOP)/lecture_code/review.py
+++ Courses/cs50P/lecture8(OOP)/lecture_code/review.py
@@ -1,40 +1,24 @@
-import random
-
-
-class Wizard:
-    houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
-
-    def __init__(self, name):
-        if not name:
-            raise ValueError("Missing name")
-        self.name = name
+class Vault:
+    def __init__(self, galleons=0, sickles=0, knuts=0):
+        self.galleons = galleons
+        self.sickles = sickles
+        self.knuts = knuts
 
     def __str__(self):
-        return f"{self.name} is a wizard"
+        return f"{self.galleons} Galleons, {self.sickles} Sickles, {self.knuts} Knuts"
+
+    def __add__(self, other):
+        galleons = self.galleons + other.galleons
+        sickles = self.sickles + other.sickles
+        knuts = self.knuts + other.knuts
+        return Vault(galleons, sickles, knuts)
 
 
-class Student(Wizard):
-    def __init__(self, name, house):
-        super().__init__(name)
-        self.house = house
+potter = Vault(100, 50, 25)
+print(potter)
 
-    def __str__(self):
-        return f"{self.name} is a student from house {self.house}"
+weasley = Vault(25, 50, 100)
+print(weasley)
 
-
-class Professor(Wizard):
-    def __init__(self, name, subject):
-        super().__init__(name)
-        self.subject = subject
-
-    def __str__(self):
-        return f"{self.name} is a Professor that teaches {self.subject}"
-
-
-wizard = Wizard("Albus")
-student = Student("Harry", "Gryffindor")
-professor = Professor("Severus", "Defense Against the Dark Arts")
-
-print(wizard)
-print(student)
-print(professor)
+combined = potter + weasley
+print(combined)
