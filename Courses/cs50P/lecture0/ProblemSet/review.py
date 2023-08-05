2c7d319ccad28ff5d9fd7a1e553a0c1845558497
Justin Sherrell <Superjustin44@gmail.com>
2023-08-04 18:22:54 -0700

    update

--- Courses/cs50P/lecture0/ProblemSet/review.py
+++ Courses/cs50P/lecture0/ProblemSet/review.py
@@ -1,11 +1,20 @@
-def playback(user_input):
-    return user_input.replace(" ", "...")
-
-
 def main():
-    user_input = input("Input String: ")
-    user_input = playback(user_input)
-    print(user_input)
+    dollars = dollars_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to tip "))
+    tip = dollars * percent
+    print(f"Leave ${tip:.2f}")
+
+
+def dollars_to_float(dollars):
+    dollars = dollars.removeprefix('$')
+    dollars = dollars.removesuffix('$')
+    return float(dollars)
+
+
+def percent_to_float(percent):
+    percent = percent.removeprefix('%')
+    percent = percent.removesuffix('%')
+    return float(percent) / 100
 
 
 if __name__ == "__main__":
--- Courses/cs50P/lecture0/ProblemSet/tip.py
+++ Courses/cs50P/lecture0/ProblemSet/tip.py
@@ -1,18 +1,21 @@
 def main():
     dollars = dollars_to_float(input("How much was the meal? "))
-    percent = percent_to_float(input("What percentage would you like to tip? "))
+    percent = percent_to_float(input("What percentage would you like to tip "))
     tip = dollars * percent
     print(f"Leave ${tip:.2f}")
 
 
-def dollars_to_float(d):
-    d = d.removeprefix('$')
-    return float(d)
+def dollars_to_float(dollars):
+    dollars = dollars.removeprefix('$')
+    dollars = dollars.removesuffix('$')
+    return float(dollars)
 
 
-def percent_to_float(p):
-    p = p.removesuffix('%')
-    return float(p) / 100
+def percent_to_float(percent):
+    percent = percent.removeprefix('%')
+    percent = percent.removesuffix('%')
+    return float(percent) / 100
 
 
-main()
+if __name__ == "__main__":
+    main()
