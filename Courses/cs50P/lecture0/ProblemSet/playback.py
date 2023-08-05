ea22b1aa169c8b1ad5c91d58b60e779e9c6515c9
Justin Sherrell <Superjustin44@gmail.com>
2023-08-04 18:17:30 -0700

    update

--- Courses/cs50P/lecture0/ProblemSet/playback.py
+++ Courses/cs50P/lecture0/ProblemSet/playback.py
@@ -1,3 +1,12 @@
-str = input("Input string: ")
-str = str.replace(" ", "...")
-print(str)
+def playback(user_input):
+    return user_input.replace(" ", "...")
+
+
+def main():
+    user_input = input("Input String: ")
+    user_input = playback(user_input)
+    print(user_input)
+
+
+if __name__ == "__main__":
+    main()
--- Courses/cs50P/lecture0/ProblemSet/review.py
+++ Courses/cs50P/lecture0/ProblemSet/review.py
@@ -1,3 +1,12 @@
-user_input = input("Input String: ")
+def playback(user_input):
+    return user_input.replace(" ", "...")
 
-print(user_input.casefold())
+
+def main():
+    user_input = input("Input String: ")
+    user_input = playback(user_input)
+    print(user_input)
+
+
+if __name__ == "__main__":
+    main()
