a52a6a03f9786e94af90d28f6b03b9a9a69203c0
Justin Sherrell <Superjustin44@gmail.com>
2023-08-05 18:09:11 -0700

    update

--- Courses/cs50P/lecture1/problem_set_1/meal.py
+++ Courses/cs50P/lecture1/problem_set_1/meal.py
@@ -1,18 +1,23 @@
-def main():
-    userInput = input("What time is it? ")
-    converted = convert(userInput)
-
-    if 7.0 <= converted <= 8.0:
-        print("Breakfast time")
-    elif 12.0 <= converted <= 13.0:
-        print("Lunch time")
-    elif 18.0 <= converted <= 19.0:
-        print("Dinner time")
-
-
 def convert(time):
-    hours, minutes = time.split(":")
+    hours, minutes = time.split(':')
     return float(hours) + float(minutes) / 60
+
+
+def what_time(time):
+    if 7.0 <= time <= 8.0:
+        return "Breakfast time"
+    elif 12.0 <= time <= 13.0:
+        return "Lunch time"
+    elif 18.0 <= time <= 19.0:
+        return "Dinner time"
+
+
+def main():
+    user_input = input("What time is it? ")
+    converted_time = convert(user_input)
+    meal_time = what_time(converted_time)
+    if meal_time:
+        print(meal_time)
 
 
 if __name__ == "__main__":
--- Courses/cs50P/lecture1/problem_set_1/review.py
+++ Courses/cs50P/lecture1/problem_set_1/review.py
@@ -1,12 +1,23 @@
-def calculate(expression):
-    num1, operator, num2 = expression.split(' ')
-    return float(eval(num1 + operator + num2))
+def convert(time):
+    hours, minutes = time.split(':')
+    return float(hours) + float(minutes) / 60
+
+
+def what_time(time):
+    if 7.0 <= time <= 8.0:
+        return "Breakfast time"
+    elif 12.0 <= time <= 13.0:
+        return "Lunch time"
+    elif 18.0 <= time <= 19.0:
+        return "Dinner time"
 
 
 def main():
-    expression = input("Expression: ")
-    result = calculate(expression)
-    print(f"{round(result, 1)}")
+    user_input = input("What time is it? ")
+    converted_time = convert(user_input)
+    meal_time = what_time(converted_time)
+    if meal_time:
+        print(meal_time)
 
 
 if __name__ == "__main__":
