efed6264f16942a16795b23200728e0e7431c37f
Justin Sherrell <Superjustin44@gmail.com>
2023-08-06 15:21:00 -0700

    update

--- Courses/cs50P/lecture1/problem_set_1/review.py
+++ Courses/cs50P/lecture1/problem_set_1/review.py
@@ -1,23 +1,16 @@
-def convert(time):
-    hours, minutes = time.split(':')
-    return float(hours) + float(minutes) / 60
-
-
-def what_time(time):
-    if 7.0 <= time <= 8.0:
-        return "Breakfast time"
-    elif 12.0 <= time <= 13.0:
-        return "Lunch time"
-    elif 18.0 <= time <= 19.0:
-        return "Dinner time"
+def greetings(user_input):
+    if "hello" in user_input:
+        return "$0"
+    elif user_input.startswith("h"):
+        return "$20"
+    else:
+        return "$100"
 
 
 def main():
-    user_input = input("What time is it? ")
-    converted_time = convert(user_input)
-    meal_time = what_time(converted_time)
-    if meal_time:
-        print(meal_time)
+    user_input = input("Greeting: ")
+    pay_out = greetings(user_input.lower())
+    print(pay_out)
 
 
 if __name__ == "__main__":
