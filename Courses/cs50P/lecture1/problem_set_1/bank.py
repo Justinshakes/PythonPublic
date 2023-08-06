552ffd4f2e5a05889f5b2aa504f49142eb8c8796
Justin Sherrell <Superjustin44@gmail.com>
2023-08-06 15:23:04 -0700

    Update bank.py

--- Courses/cs50P/lecture1/problem_set_1/bank.py
+++ Courses/cs50P/lecture1/problem_set_1/bank.py
@@ -1,7 +1,7 @@
 def greetings(user_input):
     if "hello" in user_input:
         return "$0"
-    elif user_input.startswith('h'):
+    elif user_input.startswith("h"):
         return "$20"
     else:
         return "$100"
@@ -9,7 +9,8 @@ def greetings(user_input):
 
 def main():
     user_input = input("Greeting: ")
-    print(greetings(user_input.lower().strip()))
+    pay_out = greetings(user_input.lower())
+    print(pay_out)
 
 
 if __name__ == "__main__":
