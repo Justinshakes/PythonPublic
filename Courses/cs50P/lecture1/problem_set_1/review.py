14745ae29edc569d6cc7eb7d8e621c1874674797
Justin Sherrell <Superjustin44@gmail.com>
2023-08-07 21:19:05 -0700

    Update review.py

--- Courses/cs50P/lecture1/problem_set_1/review.py
+++ Courses/cs50P/lecture1/problem_set_1/review.py
@@ -1,15 +1,23 @@
-def greetings(user_input):
-    if "hello" in user_input:
-        return "$0"
-    elif user_input.startswith('h'):
-        return "$20"
-    return "$100"
+def check_file_type(file):
+    file_type_mapping = {
+        "gif": "image/gif",
+        "png": "image/png",
+        "jpg": "image/jpeg",
+        "jpeg": "image/jpeg",
+        "pdf": "application/pdf",
+        "txt": "text/plain",
+        "zip": "application/zip",
+    }
+
+    file_array = file.split(".")
+    file_type = file_array[-1].lower().strip()
+
+    return file_type_mapping.get(file_type, "application/octet-stream")
 
 
 def main():
-    user_input = input("Greeting: ")
-    pay_out = greetings(user_input.strip().lower())
-    print(pay_out)
+    file = input("File name: ")
+    print(check_file_type(file))
 
 
 if __name__ == "__main__":
