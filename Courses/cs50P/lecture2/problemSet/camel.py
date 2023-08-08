42093421fb31209d0e0e735a731780067eb6a074
Justin Sherrell <Superjustin44@gmail.com>
2023-08-08 16:52:40 -0700

    camel.py

--- Courses/cs50P/lecture2/problemSet/camel.py
+++ Courses/cs50P/lecture2/problemSet/camel.py
@@ -1,18 +1,20 @@
-def foo(string):
-    vowels = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"]
+def convert_to_snake_case(string):
     result = ""
 
     for char in string:
-        if char not in vowels:
+        if char.isupper():
+            result += "_" + char.lower()
+        else:
             result += char
 
     return result
 
 
 def main():
-    user_string = input("Twitter Input: ")
-    result = foo(user_string)
-    print(result)
+    user_input = input("camelCase: ")
+
+    snake_case = convert_to_snake_case(user_input)
+    print(snake_case)
 
 
 if __name__ == "__main__":
--- Courses/cs50P/lecture2/problemSet/review.py
+++ Courses/cs50P/lecture2/problemSet/review.py
@@ -1,39 +1,20 @@
-def is_valid(plate):
-    return check_length(plate) and check_first_two(plate) and check_numbers_at_end(plate) and check_no_punctuation(plate)
+def convert_to_snake_case(string):
+    result = ""
 
+    for char in string:
+        if char.isupper():
+            result += "_" + char.lower()
+        else:
+            result += char
 
-def check_length(plate):
-    return 2 <= len(plate) <= 6
-
-
-def check_first_two(plate):
-    return plate[:2].isalpha()
-
-
-def check_numbers_at_end(plate):
-    found_number = False
-
-    for i in range(2, len(plate)):
-        if plate[i].isnumeric() and not found_number and plate[i] == '0':
-            return False
-        if plate[i].isalpha() and found_number:
-            return False
-        if plate[i].isnumeric() and not found_number:
-            found_number = True
-
-    return True
-
-
-def check_no_punctuation(plate):
-    return plate.isalnum()
+    return result
 
 
 def main():
-    plate = input("Plate: ")
-    if is_valid(plate):
-        print("Valid")
-    else:
-        print("Invalid")
+    user_input = input("camelCase: ")
+
+    snake_case = convert_to_snake_case(user_input)
+    print(snake_case)
 
 
 if __name__ == "__main__":
