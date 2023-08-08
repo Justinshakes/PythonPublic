785a0b1392d6543ad86f6253bffb0267fdf51cec
Justin Sherrell <Superjustin44@gmail.com>
2023-08-07 21:47:48 -0700

    update

--- Courses/cs50P/lecture2/problemSet/camel.py
+++ Courses/cs50P/lecture2/problemSet/camel.py
@@ -1,30 +1,19 @@
-def main():
-    # Prompt the user to enter a camelCase string
-    camelCase = input("camelCase: ")
-
-    # Convert the camelCase string to snake_case
-    snake_case = convert_to_snake_case(camelCase)
-
-    print(snake_case)
-
-
-def convert_to_snake_case(string):
-    # Initialize an empty string to store the snake_case result
+def foo(string):
+    vowels = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"]
     result = ""
 
-    # Iterate over each character in the input string
     for char in string:
-        # Check if the current character is uppercase
-        if char.isupper():
-            # If the character is uppercase, add an underscore before it and convert it to lowercase
-            result += "_" + char.lower()
-        else:
-            # If the character is not uppercase, simply add it to the result string
+        if char not in vowels:
             result += char
 
-    # Return the snake_case string
     return result
 
 
-# Call the main function to start the program
-main()
+def main():
+    user_string = input("Twitter Input: ")
+    result = foo(user_string)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
--- Courses/cs50P/lecture2/problemSet/twttr.py
+++ Courses/cs50P/lecture2/problemSet/twttr.py
@@ -1,23 +1,19 @@
-def main():
-    # Prompt the user for input and store it in user_string variable
-    user_string = input("Input: ")
+def foo(string):
+    vowels = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"]
+    result = ""
 
-    # Define a list of vowels (both uppercase and lowercase)
-    vowels = ['A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u']
-
-    # Initialize an empty string to store the modified result
-    result_string = ''
-
-    # Iterate over each character in the user's input string
-    for char in user_string:
-        # Check if the character is not in the list of vowels
+    for char in string:
         if char not in vowels:
-            # Append the character to the result_string
-            result_string += char
+            result += char
 
-    # Print the modified string without the vowels
-    print(result_string)
+    return result
 
 
-# Call the main function to start the program
-main()
+def main():
+    user_string = input("Twitter Input: ")
+    result = foo(user_string)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
