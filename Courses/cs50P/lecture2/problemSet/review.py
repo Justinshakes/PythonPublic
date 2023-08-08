31f539bd1606b9bf8204ea45e6d7d09a64f43de2
Justin Sherrell <Superjustin44@gmail.com>
2023-08-07 21:42:21 -0700

    Update review.py

--- Courses/cs50P/lecture2/problemSet/review.py
+++ Courses/cs50P/lecture2/problemSet/review.py
@@ -1,20 +1,18 @@
-def convert_to_snake_case(string):
-    result = ""
-
-    for char in string:
-        if char.isupper():
-            result += "_" + char.lower()
+def get_change(amount_due):
+    while True:
+        n = int(input("Insert Coin: "))
+        if n == 5 or n == 10 or n == 25:
+            return n
         else:
-            result += char
-
-    return result
+            print("Amount Due: ", amount_due, '\xa2', sep="")
 
 
 def main():
-    user_input = input("camelCase: ")
-
-    snake_case = convert_to_snake_case(user_input)
-    print(snake_case)
+    amount_due = 50
+    while amount_due > 0:
+        print("Amount Due: ", amount_due, '\xa2', sep="")
+        amount_due -= get_change(amount_due)
+    print("Change Owed: ", abs(amount_due), '\xa2', sep="")
 
 
 if __name__ == "__main__":
