b1b2ff65cfd38160ea1e40ff2bfab5ff149f897f
Justin Sherrell <Superjustin44@gmail.com>
2023-08-07 21:28:15 -0700

    Update review.py

---
+++ Courses/cs50P/lecture1/problem_set_1/review.py
@@ -0,0 +1,25 @@
+def convert(time):
+    hours, minutes = time.split(":")
+    return float(hours) + float(minutes) / 60
+
+
+def what_time(time):
+    if 7 <= time <= 8:
+        return "Breakfast time"
+    elif 12 <= time <= 13:
+        return "Lunch time"
+    elif 18 <= time <= 19:
+        return "Dinner time"
+    else:
+        return ""
+
+
+def main():
+    user_input = input("What time is it? ")
+    converted_time = convert(user_input)
+    meal_time = what_time(converted_time)
+    print(meal_time)
+
+
+if __name__ == "__main__":
+    main()
