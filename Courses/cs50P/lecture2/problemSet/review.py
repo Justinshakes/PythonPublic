e14142aaf42ef4bbafe1fc4330fe88615c58628c
Justin Sherrell <Superjustin44@gmail.com>
2023-08-07 21:46:42 -0700

    Update review.py

--- Courses/cs50P/lecture2/problemSet/review.py
+++ Courses/cs50P/lecture2/problemSet/review.py
@@ -1,18 +1,18 @@
-def get_change(amount_due):
-    while True:
-        n = int(input("Insert Coin: "))
-        if n == 5 or n == 10 or n == 25:
-            return n
-        else:
-            print("Amount Due: ", amount_due, '\xa2', sep="")
+def foo(string):
+    vowels = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"]
+    result = ""
+
+    for char in string:
+        if char not in vowels:
+            result += char
+
+    return result
 
 
 def main():
-    amount_due = 50
-    while amount_due > 0:
-        print("Amount Due: ", amount_due, '\xa2', sep="")
-        amount_due -= get_change(amount_due)
-    print("Change Owed: ", abs(amount_due), '\xa2', sep="")
+    user_string = input("Twitter Input: ")
+    result = foo(user_string)
+    print(result)
 
 
 if __name__ == "__main__":
