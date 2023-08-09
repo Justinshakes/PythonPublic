b64afe6b8666ddfcb9c06c255833748e1e974a8a
Justin Sherrell <Superjustin44@gmail.com>
2023-08-08 19:44:49 -0700

    plates.py

--- Courses/cs50P/lecture2/problemSet/plates.py
+++ Courses/cs50P/lecture2/problemSet/plates.py
@@ -1,45 +1,46 @@
+def is_valid(plate):
+    return (
+        check_length(plate)
+        and check_first_two(plate)
+        and check_numbers_at_end(plate)
+        and check_no_punctuation(plate)
+    )
+
+
+def check_length(plate):
+    return 2 <= len(plate) <= 6
+
+
+def check_first_two(plate):
+    return plate[:2].isalpha()
+
+
+def check_numbers_at_end(plate):
+    found_number = False
+
+    for i in range(2, len(plate)):
+        if plate[i].isnumeric() and not found_number and plate[i] == "0":
+            return False
+        if plate[i].isalpha() and found_number:
+            return False
+        if plate[i].isnumeric() and not found_number:
+            found_number = True
+
+    return True
+
+
+def check_no_punctuation(plate):
+    return plate.isalnum()
+
+
 def main():
-    # Prompt the user for a license plate input
     plate = input("Plate: ")
+
     if is_valid(plate):
         print("Valid")
     else:
         print("Invalid")
 
 
-def is_valid(s):
-    # Check if the license plate is valid by calling the individual checks
-    return check_length(s) and check_first_two(s) and check_numbers_at_end(s) and check_no_punctuation(s)
-
-
-def check_length(s):
-    # Check if the length of the license plate is between 2 and 6 characters
-    return 2 <= len(s) <= 6
-
-
-def check_first_two(s):
-    # Check if the first two characters of the license plate are alphabetic letters
-    return s[:2].isalpha()
-
-
-def check_numbers_at_end(plate):
-    # Check if numbers appear at the end of the license plate and not in the middle
-    found_number = False
-
-    for i in range(2, len(plate)):
-        if plate[i].isnumeric() and not found_number and plate[i] == '0':
-            return False  # Return False if the first number used is '0'
-        if plate[i].isalpha() and found_number:
-            return False  # Return False if a letter appears after a number
-        if plate[i].isnumeric() and not found_number:
-            found_number = True  # Set found_number to True when the first number is encountered
-
-    return True
-
-
-def check_no_punctuation(s):
-    # Check if the license plate contains only alphabetic letters or digits
-    return s.isalnum()
-
-
-main()
+if __name__ == "__main__":
+    main()
--- Courses/cs50P/lecture2/problemSet/review.py
+++ Courses/cs50P/lecture2/problemSet/review.py
@@ -1,18 +1,45 @@
-def twitter_string(tweet):
-    vowels = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"]
-    result = ""
+def is_valid(plate):
+    return (
+        check_length(plate)
+        and check_first_two(plate)
+        and check_numbers_at_end(plate)
+        and check_no_punctuation(plate)
+    )
 
-    for char in tweet:
-        if char not in vowels:
-            result += char
 
-    return result
+def check_length(plate):
+    return 2 <= len(plate) <= 6
+
+
+def check_first_two(plate):
+    return plate[:2].isalpha()
+
+
+def check_numbers_at_end(plate):
+    found_number = False
+
+    for i in range(2, len(plate)):
+        if plate[i].isnumeric() and not found_number and plate[i] == "0":
+            return False
+        if plate[i].isalpha() and found_number:
+            return False
+        if plate[i].isnumeric() and not found_number:
+            found_number = True
+
+    return True
+
+
+def check_no_punctuation(plate):
+    return plate.isalnum()
 
 
 def main():
-    tweet = input("Tweet: ")
-    result = twitter_string(tweet)
-    print(result)
+    plate = input("Plate: ")
+
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
 
 
 if __name__ == "__main__":
