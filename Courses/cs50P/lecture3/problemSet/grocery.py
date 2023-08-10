8dd4e5f2d5e24340f2e7f785cc928cad91b2b63b
Justin Sherrell <Superjustin44@gmail.com>
2023-08-09 19:17:00 -0700

    grocery

--- Courses/cs50P/lecture3/problemSet/grocery.py
+++ Courses/cs50P/lecture3/problemSet/grocery.py
@@ -1,20 +1,27 @@
-grocery = {}
-while True:
-    try:
-        item = input().upper()
+def create_grocery_list():
+    grocery_list = {}
+    while True:
+        try:
+            item = input().upper()
 
-        if item in grocery:
-            grocery[item] += 1
-        else:
-            grocery[item] = 1
-    except EOFError:
-        break
-
-# print(grocery.keys())
-# print(grocery.values())
-# print(grocery.items())
-
-for key in sorted(grocery.keys()):
-    print(grocery[key], key)
+            if item in grocery_list:
+                grocery_list[item] += 1
+            else:
+                grocery_list[item] = 1
+        except EOFError:
+            return grocery_list
 
 
+def main():
+    grocery_list = create_grocery_list()
+
+    # print(grocery.keys())
+    # print(grocery.values())
+    # print(grocery.items())
+
+    for key in sorted(grocery_list.keys()):
+        print(grocery_list[key], key)
+
+
+if __name__ == "__main__":
+    main()
