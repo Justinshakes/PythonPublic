084876def4a33ef268c11b23dbb01169e1cb0bf0
Justin Sherrell <Superjustin44@gmail.com>
2023-08-09 17:42:18 -0700

    fuel.py

---
+++ Courses/cs50P/lecture3/problemSet/review.py
@@ -0,0 +1,33 @@
+def get_fraction():
+    while True:
+        fraction = input("Fraction: ")
+        try:
+            result = round(eval(fraction), 2)
+            if 0 <= result <= 1:
+                return result
+        except (NameError, ZeroDivisionError, SyntaxError, ValueError):
+            pass
+
+
+def get_percentage(fraction):
+    return round(float(fraction) * 100)
+
+
+def fuel_gauge(percentage):
+    if percentage <= 1:
+        return "E"
+    elif percentage >= 99:
+        return "F"
+    else:
+        return f"{percentage}%"
+
+
+def main():
+    fraction = get_fraction()
+    percentage = get_percentage(fraction)
+    fuel_percentage = fuel_gauge(percentage)
+    print(fuel_percentage)
+
+
+if __name__ == "__main__":
+    main()
