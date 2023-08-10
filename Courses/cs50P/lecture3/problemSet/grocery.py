c7ca0ab6291834ba267a6c4c8456d0908d94e280
Justin Sherrell <Superjustin44@gmail.com>
2023-08-09 18:22:23 -0700

    grocery

--- Courses/cs50P/lecture3/problemSet/grocery.py
+++ Courses/cs50P/lecture3/problemSet/grocery.py
@@ -1,25 +1,20 @@
-grocery_list = {}  # Create an empty dictionary to store the grocery items and their occurrences
-
+grocery = {}
 while True:
     try:
-        item = input().upper()  # Prompt the user to enter an item and convert it to uppercase
+        item = input().upper()
 
-        if item in grocery_list:  # Check if the item is already present in the grocery list
-            grocery_list[item] += 1  # If yes, increment the occurrence count of the item by 1
+        if item in grocery:
+            grocery[item] += 1
         else:
-            grocery_list[item] = 1  # If not, add the item to the grocery list with an initial count of 1
-
+            grocery[item] = 1
     except EOFError:
-        break  # Exit the loop immediately when Ctrl+D (EOF) is encountered
+        break
 
-# Sort the grocery list dictionary alphabetically by item
-sorted_list = sorted(grocery_list.items(), key=lambda x: x[0])
+# print(grocery.keys())
+# print(grocery.values())
+# print(grocery.items())
 
-for item, count in sorted_list:  # Iterate over the sorted items and their counts
-    print(f"{count} {item}")  # Print the count and the item in the desired format
+for key in sorted(grocery.keys()):
+    print(grocery[key], key)
 
-"""
-    item: This variable holds the value of the item (key) from the sorted list. It represents the name of the grocery item.
 
-count: This variable holds the value of the count (value) associated with the item from the sorted list. It represents the number of occurrences of the grocery item.
-"""
--- Courses/cs50P/lecture3/problemSet/review.py
+++ Courses/cs50P/lecture3/problemSet/review.py
@@ -1,34 +1,20 @@
-def menu(item):
-    menu_items = {
-        "Baja Taco": 4.00,
-        "Burrito": 7.50,
-        "Bowl": 8.50,
-        "Nachos": 11.00,
-        "Quesadilla": 8.50,
-        "Super Burrito": 8.50,
-        "Super Quesadilla": 9.50,
-        "Taco": 3.00,
-        "Tortilla Salad": 8.00,
-    }
-    return menu_items.get(item, 0)
+grocery = {}
+while True:
+    try:
+        item = input().upper()
+
+        if item in grocery:
+            grocery[item] += 1
+        else:
+            grocery[item] = 1
+    except EOFError:
+        break
+
+# print(grocery.keys())
+# print(grocery.values())
+# print(grocery.items())
+
+for key in sorted(grocery.keys()):
+    print(grocery[key], key)
 
 
-def main():
-    total = 0.00
-    print("Ctrl+D to stop ordering")
-    while True:
-        try:
-            item = input("Item: ")
-            item_cost = menu(item.title())
-            if item_cost == 0:
-                print(f"{item} is not on the menu")
-            total += item_cost
-            print(f"Total: ${total:.2f}")
-
-        except EOFError:
-            break
-    print(f"Grand total: ${total:.2f}")
-
-
-if __name__ == "__main__":
-    main()
