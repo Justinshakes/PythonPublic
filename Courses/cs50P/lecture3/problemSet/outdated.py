4922bfedc2ff925266bacaf7ee2c427230b7d10d
Justin Sherrell <Superjustin44@gmail.com>
2023-08-13 18:02:09 -0700

    outdated.py

--- Courses/cs50P/lecture3/problemSet/outdated.py
+++ Courses/cs50P/lecture3/problemSet/outdated.py
@@ -2,25 +2,7 @@ def get_date_input():
     return input("Enter a date (MM/DD/YYYY or Month Day, Year): ")
 
 
-def parse_date_input(date_input, months):
-    if '/' in date_input:
-        return map(int, date_input.split('/'))
-    else:
-        month_name, day, year = date_input.split()
-        day = day.replace(',', '')
-        month = months.index(month_name) + 1
-        return int(day), int(month), int(year)
-
-
-def validate_date(month, day):
-    return 1 <= month <= 12 and 1 <= day <= 31
-
-
-def format_date(year, month, day):
-    return f"{year:04d}-{month:02d}-{day:02d}"
-
-
-def main():
+def parse_date_input(date_input):
     months = (
         "January",
         "February",
@@ -35,11 +17,28 @@ def main():
         "November",
         "December",
     )
+    if "/" in date_input:
+        return map(int, date_input.split("/"))
+    else:
+        month_name, day, year = date_input.split()
+        day = day.replace(",", "")
+        month = months.index(month_name) + 1
+        return int(day), int(month), int(year)
 
+
+def validate_date(month, day):
+    return 1 <= month <= 12 and 1 <= day <= 31
+
+
+def format_date(year, month, day):
+    return f"{year:04d}-{month:02d}-{day:02d}"
+
+
+def main():
     while True:
         try:
             date_input = get_date_input()
-            day, month, year = parse_date_input(date_input, months)
+            day, month, year = parse_date_input(date_input)
 
             if validate_date(month, day):
                 date_output = format_date(year, month, day)
@@ -48,7 +47,7 @@ def main():
             else:
                 print("Invalid date. Please enter a valid date.")
         except ValueError:
-            print("Invalid input. Please enter the date in the specified format.")
+            print("Invalid input. Please enter the date in the specified format. ")
 
 
 if __name__ == "__main__":
--- Courses/cs50P/lecture3/problemSet/review.py
+++ Courses/cs50P/lecture3/problemSet/review.py
@@ -1,31 +1,53 @@
-def create_grocery_list():
-    grocery_list = {}
-    while True:
-        try:
-            item = input().upper()
-
-            if item in grocery_list:
-                grocery_list[item] += 1
-            else:
-                grocery_list[item] = 1
-
-        except EOFError:
-            return grocery_list
+def get_date_input():
+    return input("Enter a date (MM/DD/YYYY or Month Day, Year): ")
 
 
-def print_list(grocery_list):
-    print("keys: ", grocery_list.keys())
-    print("values: ", grocery_list.values())
-    print("items: ", grocery_list.items())
+def parse_date_input(date_input):
+    months = (
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    )
+    if "/" in date_input:
+        return map(int, date_input.split("/"))
+    else:
+        month_name, day, year = date_input.split()
+        day = day.replace(",", "")
+        month = months.index(month_name) + 1
+        return int(day), int(month), int(year)
 
-    for key in sorted(grocery_list.keys()):
-        print(grocery_list[key], key)
+
+def validate_date(month, day):
+    return 1 <= month <= 12 and 1 <= day <= 31
+
+
+def format_date(year, month, day):
+    return f"{year:04d}-{month:02d}-{day:02d}"
 
 
 def main():
-    print("Create your grocery list: ")
-    grocery_list = create_grocery_list()
-    print_list(grocery_list)
+    while True:
+        try:
+            date_input = get_date_input()
+            day, month, year = parse_date_input(date_input)
+
+            if validate_date(month, day):
+                date_output = format_date(year, month, day)
+                print(date_output)
+                break
+            else:
+                print("Invalid date. Please enter a valid date.")
+        except ValueError:
+            print("Invalid input. Please enter the date in the specified format. ")
 
 
 if __name__ == "__main__":
