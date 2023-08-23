43e0142c17d2942b78554c1da7e4b12f4cce60f9
Justin Sherrell <Superjustin44@gmail.com>
2023-08-23 16:13:42 -0700

    update

--- Courses/cs50P/lecture1/problem_set_1/review.py
+++ Courses/cs50P/lecture1/problem_set_1/review.py
@@ -1,3 +1,17 @@
-s = "hi!"
+def find_multiples(integer, limit):
+    multiples = []
+    tracker = integer
 
-s = s.removesuffix('!')
+    while tracker <= limit:
+        multiples.append(tracker)
+        tracker += integer
+
+    return multiples
+
+
+def main():
+    multiples = find_multiples(2, 6)
+    print(multiples)
+
+
+main()
