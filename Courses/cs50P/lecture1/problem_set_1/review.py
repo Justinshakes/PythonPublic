4441ccea56e326bbfc4e162fb0e3b8e02a9186db
Justin Sherrell <Superjustin44@gmail.com>
2023-08-23 16:07:48 -0700

    remove suffix

--- Courses/cs50P/lecture1/problem_set_1/review.py
+++ Courses/cs50P/lecture1/problem_set_1/review.py
@@ -1,24 +1,3 @@
-def convert(time):
-    hours, minutes = time.split(':')
-    return round(float(hours) + float(minutes) / 60, 2)
+s = "hi!"
 
-
-def what_time(time):
-    if 7 <= time <= 8:
-        return "Breakfast time"
-    elif 12 <= time <= 13:
-        return "Lunch time"
-    elif 18 <= time <= 19:
-        return "Dinner time"
-    else:
-        return ""
-
-
-def main():
-    user_input = input("What time is it in 24 hour format: ")
-    converted_time = convert(user_input)
-    meal_time = what_time(converted_time)
-
-
-if __name__ == "__main__":
-    main()
+s = s.removesuffix('!')
