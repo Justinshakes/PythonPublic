d603f9af503a366f64e1a4038cfe6a30c45db22e
Justin Sherrell <Superjustin44@gmail.com>
2023-08-24 21:20:54 -0700

    max() combat

--- testingArea/codewars.py
+++ testingArea/codewars.py
@@ -1,18 +1,13 @@
-def say_hello(name, city, state):
-    formatted_name = ' '.join(name)
-    return f"Hello, {formatted_name}! Welcome to {city}, {state}!"
+def combat(health, damage):
+    remaining_health = max(health - damage, 0)
+    return remaining_health
+
 
 def main():
-    result = say_hello(["John", "Smith"], "Phoenix", "Arizona")
-    result2 = say_hello(["Wallace", "Russel", "Osbourne"], "Albany", "New York")
-
-    print(result)
+    result1 = combat(100, 5)
+    result2 = combat(20, 30)
+    print(result1)
     print(result2)
 
-    result3 = say_hello(["Former", "President", "Walker"], "Los Angeles", "California")
-    result4 = say_hello(["Former", "President", "Garrett", "Walker"], "Los Angeles", "California")
-
-    print(result3)
-    print(result4)
 
 main()
