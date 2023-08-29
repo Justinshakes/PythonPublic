4fd101ed3740d1f6bdbbe7906122764650cc8706
Justinshakes <Superjustin44@gmail.com>
2023-08-28 21:42:38 -0700

    Update solution.py

--- testingArea/codewars/solution.py
+++ testingArea/codewars/solution.py
@@ -1,14 +1,10 @@
-def process_string(input_str):
-    sep_list = input_str.split(",")
-    if len(sep_list) < 3:
-        return None
-    processed_str = " ".join(sep_list[1:-1])
-    return processed_str
+def add_length(str_):
+    list = str_.split()
+    result = []
+
 
 def main():
-    input_str = '1,2,3'
-    processed_str = process_string(input_str)
-    print(processed_str)
+    print(add_length("apple ban"))
 
 if __name__ == "__main__":
     main()
