fc9a0eec8e5a38606d7a3a013af466077c06ece2
Justin Sherrell <Superjustin44@gmail.com>
2023-09-03 22:14:34 -0700

    Update solution.py

--- testingArea/codewars/solution.py
+++ testingArea/codewars/solution.py
@@ -1,26 +1,16 @@
-def people_with_age_drink(age):
-    if age < 14:
-        return "drink toddy"
-    if age < 18:
-        return "drink coke"
-    if age < 21:
-        return "drink beer"
-    else:
-        return "drink whisky"
+def invert(lst):
+    result = []
+    for num in lst:
+        if num <= 0:
+            result.append(abs(num))
+        else:
+            result.append(num * -1)
+    return result
+
 
 def main():
-    # Test cases
-    result1 = people_with_age_drink(13)
-    result2 = people_with_age_drink(17)
-    result3 = people_with_age_drink(18)
-    result4 = people_with_age_drink(20)
-    result5 = people_with_age_drink(30)
+    result1 = invert([1, -2, 3, -4, 5])  # [-1,2,-3,4,-5
+    print(result1)
 
-    # Print results
-    print(result1)  # Expected: "drink toddy"
-    print(result2)  # Expected: "drink coke"
-    print(result3)  # Expected: "drink beer"
-    print(result4)  # Expected: "drink beer"
-    print(result5)  # Expected: "drink whisky"
 
 main()
