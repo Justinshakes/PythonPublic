68aaeceb96005e880f6c64dfe18494511e0ff83c
Justin Sherrell <Superjustin44@gmail.com>
2023-09-04 19:47:13 -0700

    Update solution.py

--- testingArea/codewars/solution.py
+++ testingArea/codewars/solution.py
@@ -1,13 +1,16 @@
-def divisible_by(numbers, divisor):
+def square_or_square_root(arr):
     result = []
-    for num in numbers:
-        if num % divisor == 0:
-            result.append(num)
+    for num in arr:
+        square_root = num ** 0.5
+        if square_root.is_integer():
+            result.append(int(square_root))
+        else:
+            result.append(num ** 2)
     return result
 
 
 def main():
-    result1 = divisible_by([0,1,2,3,4,5,6], 4) # [0,4]
+    result1 = square_or_square_root([4, 3, 9, 7, 2, 1])  # [2, 9, 3, 49, 4, 1]
     print(result1)
     # print(result2)
 
