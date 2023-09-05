7738e591abdbc44f0a5e950d491a7e8022f803a2
Justin Sherrell <Superjustin44@gmail.com>
2023-09-04 19:26:37 -0700

    Update solution.py

--- testingArea/codewars/solution.py
+++ testingArea/codewars/solution.py
@@ -1,14 +1,15 @@
-def remove_every_other(my_list):
-    return my_list[::2]
+def divisible_by(numbers, divisor):
+    result = []
+    for num in numbers:
+        if num % divisor == 0:
+            result.append(num)
+    return result
 
 
 def main():
-    result1 = remove_every_other([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-    # [1, 3, 5, 7, 9])
-    result2 = remove_every_other(["Hello", "Goodbye", "Hello Again"])
-    #  ['Hello', 'Hello Again'])
+    result1 = divisible_by([0,1,2,3,4,5,6], 4) # [0,4]
     print(result1)
-    print(result2)
+    # print(result2)
 
 
 if __name__ == "__main__":
