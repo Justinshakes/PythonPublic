9d7a57667a0116808ecb0507e0645e6c9c3ff0f4
Justin Sherrell <Superjustin44@gmail.com>
2023-09-06 04:20:54 -0700

    Update solution.py

--- testingArea/codewars/solution.py
+++ testingArea/codewars/solution.py
@@ -1,16 +1,13 @@
-def square_or_square_root(arr):
-    result = []
-    for num in arr:
-        square_root = num ** 0.5
-        if square_root.is_integer():
-            result.append(int(square_root))
-        else:
-            result.append(num ** 2)
-    return result
+def is_uppercase(inp):
+    for char in inp:
+        if char.isalpha() and not char.isupper():
+            return False
+    return True
+
 
 
 def main():
-    result1 = square_or_square_root([4, 3, 9, 7, 2, 1])  # [2, 9, 3, 49, 4, 1]
+    result1 = is_uppercase("hello I AM DONALD") # False
     print(result1)
     # print(result2)
 
